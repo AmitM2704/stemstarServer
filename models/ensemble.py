c4bef57c2cf800920abf60498d760124c3f5e2bb
Amit_2704 <rockmeister50@gmail.com>
2026-05-18 17:35:52 +0530

    fixed import paths

--- models/ensemble.py
+++ models/ensemble.py
@@ -7,7 +7,6 @@ class EnsembleSeparator:
     def __init__(self):
         self.demucs = DemucsSeparator()
         #self.mdx = MDXSeparator()
-
     def process(self, input_file):
 
-        self.demucs.separate(input_file)
+        return self.demucs.separate(input_file)
